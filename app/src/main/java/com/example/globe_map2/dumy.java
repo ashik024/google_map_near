eb2b07165df9029c8fc56072c006973f95eefb03
Ashikur Rahman <ashik.roshan5@gmail.com>
2021-04-18 16:48:42 +0600

    hello2

---
+++ app/src/main/java/com/example/globe_map2/dumy.java
@@ -0,0 +1,14 @@
+package com.example.globe_map2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class dumy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dumy);
+    }
+}
